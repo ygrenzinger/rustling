1c7dcc81f4d375435aa4a956251832509433f606
Maxime GELLE <maxime.gelle@gmail.com>
2022-02-21 14:00:20 +0100

    iterator5

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -55,6 +55,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    collection
+       .iter()
+       .map(|map| count_iterator(map, value))
+       .sum()
 }
 
 #[cfg(test)]
